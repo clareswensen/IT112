816d1ad0d7024d5d320fc565d7fb010badc8eb14
clareswensen <clareswensen.seattlecolleges.com>
2022-04-27 14:13:26 -0600

    meeting view and detail view

--- club/views.py
+++ club/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resources, Event
 
 # Create your views here.
@@ -9,3 +10,10 @@ def resources(request):
     resource_list=Resources.objects.all()
     return render(request, 'club/resources.html', {'resource_list': resource_list})
 
+def meetings(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'club/meetings.html', {'meeting_list': meeting_list})
+
+def meetingDetail(request, id):
+    meeting=get_object_or_404(Meeting, pk=id)
+    return render(request, 'club/meetingdetail.html', {'meeting': meeting})
